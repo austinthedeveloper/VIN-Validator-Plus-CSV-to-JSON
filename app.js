79936a93a91faa4ccb1bbfe190e4515a693cb0d4
Austin Stewart <astewart@dcodit08.local>
2015-11-11 11:43:46 -0600

    Gave the option to change the file type after changes have been made

--- app.js
+++ app.js
@@ -1,59 +1,73 @@
 var app = angular.module('plnkrApp', ['ngMaterial', 'ngCsvImport', 'ngMessages', 'ngSanitize', 'ngCsv']);
 
 app
-  .controller("DemoController", function($scope, $http){
+  .controller("DemoController", function($scope, $http) {
 
     $scope.csv = {
       "separator": "\t",
       "header": true
     };
-    
-    $scope.radioData = [
-      { label: 'Comma', value: "," },
-      { label: 'Tabs', value: "\t" }
-    ];
-    
+
+    $scope.radioData = [{
+      label: 'Comma',
+      value: ","
+    }, {
+      label: 'Tabs',
+      value: "\t"
+    }];
+
+    $scope.fileData = [{
+      label: 'TXT',
+      value: "txt"
+    }, {
+      label: 'CSV',
+      value: "csv"
+    }];
+
     $scope.vinPattern = /^[\d\w]+$/;
-    
-    $scope.fileEach = function(object) { 
+
+    $scope.fileEach = function(object) {
       var patt = new RegExp($scope.vinPattern);
       var sliced = [];
-      
+
       $scope.hasErrors = false;
       $scope.isClean = false;
-      
+
       $scope.indexes = [];
-      for(var index in object[0]){
-          $scope.indexes.push(index);
+      for (var index in object[0]) {
+        $scope.indexes.push(index);
       }
 
-      object.forEach(function(item, index, obj){
+      object.forEach(function(item, index, obj) {
         item.VIN = item.VIN.split(' ').join('');
-        if(item.VIN.length !== 17 || !patt.test(item.VIN)) {
+        if (item.VIN.length !== 17 || !patt.test(item.VIN)) {
           sliced.push(index);
         }
       });
-      if(sliced.length) {
+      if (sliced.length) {
         $scope.sliced = sliced;
         $scope.hasErrors = true;
       } else {
         $scope.isClean = true;
       }
     };
-   
-    
-    $scope.$watch('csv.result', function(newVal, oldVal){
-        if(newVal && newVal !== oldVal) {
-            $scope.fileExtension = newVal.filename.split('.').pop();
-            $scope.badExtension = false;
-            if($scope.fileExtension === 'csv' || $scope.fileExtension === 'txt') {
-              $scope.fileEach(newVal);
-            } else {
-               $scope.badExtension = true;
-            }
+
+
+    $scope.$watch('csv.result', function(newVal, oldVal) {
+      if (newVal && newVal !== oldVal) {
+
+        $scope.fileExtension = newVal.filename.split('.').pop().toLowerCase();
+        $scope.fileName = newVal.filename.replace(/\.[^/.]+$/, "");
+
+        $scope.badExtension = false;
+        if ($scope.fileExtension === 'csv' || $scope.fileExtension === 'txt') {
+          $scope.fileEach(newVal);
+        } else {
+          $scope.badExtension = true;
         }
+      }
     });
-    
-    
-    
+
+
+
   });
